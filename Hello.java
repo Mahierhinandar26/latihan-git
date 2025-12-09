0144598e5e2be641f10ebb2590b44b238a95712b
Mahierhinandar26 <mahierhinandar26@gmail.com>
2025-12-09 21:26:20 +0700

    style: tambah komentar awal di Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         // Commit 1: Menambah komentar awal
-        System.out.println("Hello World");
+        System.out.println("Hello Dunia");
     }
 }
